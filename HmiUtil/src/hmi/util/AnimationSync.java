21b3f61e05fff1a996ff85e57f4d95104350313b
Dennis Reidsma <d.reidsma@utwente.nl>
2012-08-06 11:40:44 +0200

    package rename

--- HmiUtil/src/hmi/util/AnimationSync.java
+++ HmiUtil/src/hmi/util/AnimationSync.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Elckerlyc.  If not, see http://www.gnu.org/licenses/.
  ******************************************************************************/
-package asap.utils;
+package hmi.util;
 
 /**
  * Singleton for synchronization between animation and render threads
