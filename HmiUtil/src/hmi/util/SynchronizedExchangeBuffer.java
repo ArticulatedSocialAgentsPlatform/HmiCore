0468018098cd1ee8341efddc143ab99bc8cf16ca
zwiers <j.zwiers@utwente.nl>
2012-10-12 16:02:18 +0200

    HmiUtil ExchangeBuffer classes added

---
+++ HmiUtil/src/hmi/util/SynchronizedExchangeBuffer.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ */
+package hmi.util;
+
+/**
+ * An SynchronizedExchangeBuffer is an ExchangeBuffer that, unlike ExchangeBuffer,
+ * guarantees that its operations are synchronized. 
+ */
+public interface SynchronizedExchangeBuffer extends ExchangeBuffer {
+    
+}
