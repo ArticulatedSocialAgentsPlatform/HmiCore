2a1872b097efc81039928e9baddaf603ff1ae5f8
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-03-19 14:21:44 +0100

    added swivelcosts calculator

--- HmiNeurophysics/src/hmi/neurophysics/BiologicalSwivelCostsEvaluator.java
+++ HmiNeurophysics/src/hmi/neurophysics/BiologicalSwivelCostsEvaluator.java
@@ -14,7 +14,11 @@ public class BiologicalSwivelCostsEvaluator
     private final double maxFeasibleElbowSwivel;
     private final double sigmaOfGaussianCostsDistribution;
     private final double freedomOfTheGaussianMean;
-
+    private final double angleStepToComputeCosts = 0.1;
+    private final boolean considerBiologicalCosts = true;
+    private final boolean considerDeviationCosts = true;
+    
+    
     public BiologicalSwivelCostsEvaluator(double minFeasibleElbowSwivel, double maxFeasibleElbowSwivel,
             double sigmaOfGaussianCostsDistribution, double freedomOfTheGaussianMean)
     {
@@ -29,5 +33,51 @@ public class BiologicalSwivelCostsEvaluator
         double mu = ((minFeasibleElbowSwivel + maxFeasibleElbowSwivel) / 2) + freedomOfTheGaussianMean;
         return 1.0 - (1.0 / Math.sqrt(2 * Math.PI * Math.pow(sigmaOfGaussianCostsDistribution, 2)))
                 * Math.exp(-Math.pow(swivel - mu, 2) / (2 * Math.pow(sigmaOfGaussianCostsDistribution, 2)));
+    }
+    
+    public double getSwivelAngleWithMinCost(double formerSwivel)
+    {
+        double swivel = 0;
+        double minCost = -1;
+        for (double a = minFeasibleElbowSwivel; a <= maxFeasibleElbowSwivel; a = a + angleStepToComputeCosts)
+        {
+            double sumOfCosts = 0.0;
+            if (considerBiologicalCosts)
+            {
+                double c =  getBiologicalCostsOfElbowSwivel(a);
+                sumOfCosts += c;                
+            }
+            if (considerDeviationCosts)     // && formerSwivel!=-99)=> what does that do???
+            {
+                double maxDeviation = Math.abs(maxFeasibleElbowSwivel - minFeasibleElbowSwivel);
+                double d = Math.abs(formerSwivel - a)/maxDeviation;                
+                sumOfCosts += d;
+            }
+            /* TODO
+            if (considerSelfCollision)
+            {
+                aceVec3 elbowPos = srs->getElbowPosForSwivel(a, shoulderTransform);
+                bool hasCollision = selfCollisionDetector->elbowHasCollision(elbowPos, left_right);
+                if (hasCollision)
+                {
+                    sumOfCosts += 1.0;
+                    cache_lastSelfCollisionCosts[left_right][a] = 1.0;
+                }
+                else {
+                    foundASwivelWithoutSelfCollision = true;
+                    cache_lastSelfCollisionCosts[left_right][a] = 0.0;
+                }
+            }
+            */
+            double cost = sumOfCosts/3;
+
+            if (minCost==-1 || cost<minCost)
+            {
+                minCost = cost;
+                swivel = a;
+            }
+        }
+        
+        return swivel;
     }
 }
--- HmiNeurophysics/test/src/hmi/neurophysics/BiologicalSwivelCostsEvaluatorTest.java
+++ HmiNeurophysics/test/src/hmi/neurophysics/BiologicalSwivelCostsEvaluatorTest.java
@@ -13,11 +13,20 @@ public class BiologicalSwivelCostsEvaluatorTest
 {
     private static final double PRECISION = 0.001;
     
+    private BiologicalSwivelCostsEvaluator getVinceEvalLeft()
+    {
+        return new BiologicalSwivelCostsEvaluator(1.3, 4.44, 0.45, -0.5);
+    }
+    
+    private BiologicalSwivelCostsEvaluator getVinceEvalRight()
+    {
+        return new BiologicalSwivelCostsEvaluator(2*3.14 - 4.44, 2*3.14 + 1.3, 0.45, -0.5);
+    }
+    
     @Test
     public void testL()
     {
-        //values for Vince
-        BiologicalSwivelCostsEvaluator eval = new BiologicalSwivelCostsEvaluator(1.3, 4.44, 0.45, -0.5);
+        BiologicalSwivelCostsEvaluator eval =  getVinceEvalLeft();
         
         //values obtained from C++ implementation
         assertEquals(1,eval.getBiologicalCostsOfElbowSwivel(0),PRECISION);          //outside range : 1
@@ -28,12 +37,32 @@ public class BiologicalSwivelCostsEvaluatorTest
     @Test
     public void testR()
     {
-        //values for Vince
-        BiologicalSwivelCostsEvaluator eval = new BiologicalSwivelCostsEvaluator(2*3.14 - 4.44, 2*3.14 + 1.3, 0.45, -0.5);
+        BiologicalSwivelCostsEvaluator eval = getVinceEvalRight();
         
         //values obtained from C++ implementation
         assertEquals(1,eval.getBiologicalCostsOfElbowSwivel(0),PRECISION);          //outside range : 1
         assertEquals(0.976139,eval.getBiologicalCostsOfElbowSwivel(3),PRECISION);          
         assertEquals(0.204925,eval.getBiologicalCostsOfElbowSwivel(4),PRECISION);
+    }
+    
+    @Test
+    public void testRMinCosts()
+    {
+        BiologicalSwivelCostsEvaluator eval = getVinceEvalRight();
+        
+        assertEquals(4.14,eval.getSwivelAngleWithMinCost(0),PRECISION);
+        assertEquals(4.14,eval.getSwivelAngleWithMinCost(4.14),PRECISION);
+        assertEquals(4.14,eval.getSwivelAngleWithMinCost(-1),PRECISION);
+        assertEquals(4.24,eval.getSwivelAngleWithMinCost(6),PRECISION);
+    }
+    
+    @Test
+    public void testLMinCosts()
+    {
+        BiologicalSwivelCostsEvaluator eval = getVinceEvalLeft();
+        
+        assertEquals(2.3,eval.getSwivelAngleWithMinCost(0),PRECISION);
+        assertEquals(2.4,eval.getSwivelAngleWithMinCost(4),PRECISION);
+        assertEquals(2.3,eval.getSwivelAngleWithMinCost(1.3),PRECISION);
     }
 }
