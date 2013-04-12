0977937286d814778ca6fb1325ac53f33b5fa385
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-04-12 16:32:48 +0200

    x

---
+++ HmiTestUtil/src/hmi/testutil/demotester/FestUtils.java
@@ -0,0 +1,31 @@
+package hmi.testutil.demotester;
+
+import javax.swing.JButton;
+
+import org.fest.swing.core.GenericTypeMatcher;
+import org.fest.swing.fixture.FrameFixture;
+
+/**
+ * Some utitilities to to make Fest testing easier.
+ * @author hvanwelbergen
+ *
+ */
+public final class FestUtils
+{
+    private GenericTypeMatcher<JButton> getButtonMatcher(final String id)
+    {
+        GenericTypeMatcher<JButton> buttonMatcher = new GenericTypeMatcher<JButton>(JButton.class){
+            protected boolean isMatching(JButton jb)
+            {
+                return id.equals(jb.getText());
+            }
+        };
+        return buttonMatcher;
+    }
+    
+    
+    public void clickButton(String name, FrameFixture window)
+    {
+        window.button(getButtonMatcher(name)).click();
+    }
+}
