7555ba6b2fe56de6289976a1b54dc25a874d9656
Soufian Jebbara <sjebbara@techfak.uni-bielefeld.de>
2013-08-09 15:28:50 +0200

    joint slider adjust to Torso-tab changes

--- HmiAnimationUI/src/hmi/animationui/TorsoController.java
+++ HmiAnimationUI/src/hmi/animationui/TorsoController.java
@@ -5,6 +5,7 @@ import hmi.animation.VJoint;
 import hmi.animation.VJointUtils;
 import hmi.neurophysics.Torso;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -12,40 +13,45 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 
 /**
- * The controller handles torso rotation input from the viewer and updates the torso
+ * The controller handles torso rotation input from the viewer and updates the
+ * torso
+ * 
  * @author hvanwelbergen
  */
-public class TorsoController implements RotationsController
-{
-    private final ImmutableList<VJoint> thoracicJoints;
-    private final ImmutableList<VJoint> torsoJoints;
-    
-    
+public class TorsoController implements RotationsController {
+	private final ImmutableList<VJoint> thoracicJoints;
+	private final ImmutableList<VJoint> torsoJoints;
+	private JointController jc;
 
-    public TorsoController(VJoint model)
-    {
-        List<VJoint> joints = VJointUtils.gatherJoints(Hanim.LUMBAR_JOINTS, model);
-        thoracicJoints = ImmutableList.copyOf(VJointUtils.gatherJoints(Hanim.THORACIC_JOINTS, model));        
-        joints.addAll(thoracicJoints);        
-        torsoJoints = ImmutableList.copyOf(joints);
-    }
+	public TorsoController(VJoint model, JointController jc) {
+		this.jc = jc;
+		List<VJoint> joints = VJointUtils.gatherJoints(Hanim.LUMBAR_JOINTS,
+				model);
+		thoracicJoints = ImmutableList.copyOf(VJointUtils.gatherJoints(
+				Hanim.THORACIC_JOINTS, model));
+		joints.addAll(thoracicJoints);
+		torsoJoints = ImmutableList.copyOf(joints);
+	}
 
-    public JointView constructTorsoView()
-    {
-        return new JointView(this, ImmutableList.of("Torso ("+Joiner.on(",").join(VJointUtils.transformToSidList(torsoJoints))+")"));
-    }
+	public JointView constructTorsoView() {
+		return new JointView(this, ImmutableList.of("Torso ("
+				+ Joiner.on(",").join(
+						VJointUtils.transformToSidList(torsoJoints)) + ")"));
+	}
 
-    public void setJointRotations(Collection<JointRotationConfiguration> rotations)
-    {
-        JointRotationConfiguration config = rotations.iterator().next();
-        float q[] = new float[torsoJoints.size() * 4];
-        Torso.setTorsoRollPitchYawDegrees(q, config.getRpyDeg()[0], config.getRpyDeg()[1], config.getRpyDeg()[2], torsoJoints.size()
-                - thoracicJoints.size(), thoracicJoints.size());
-        int i = 0;
-        for (VJoint vj : torsoJoints)
-        {
-            vj.setRotation(q, i * 4);
-            i++;
-        }
-    }
+	public void setJointRotations(
+			Collection<JointRotationConfiguration> rotations) {
+		JointRotationConfiguration config = rotations.iterator().next();
+		float q[] = new float[torsoJoints.size() * 4];
+		Torso.setTorsoRollPitchYawDegrees(q, config.getRpyDeg()[0],
+				config.getRpyDeg()[1], config.getRpyDeg()[2],
+				torsoJoints.size() - thoracicJoints.size(),
+				thoracicJoints.size());
+		int i = 0;
+		for (VJoint vj : torsoJoints) {
+			vj.setRotation(q, i * 4);
+			i++;
+		}
+		jc.adjustSliderToModel(Arrays.asList(Hanim.THORACIC_JOINTS));
+	}
 }
